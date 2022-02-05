82e823480b4823066d7ce2776792318cc7680e97
Joshua Adeleke <lekejosh6wf@gmail.com>
2022-02-05 21:01:26 +0100

    fixed error

--- 01-node-tutorial/1-event-loop-examples/4-server.js
+++ 01-node-tutorial/1-event-loop-examples/4-server.js
@@ -1,10 +1,11 @@
 const http = require('http')
 
-const server = http.createServer((req, res) => {
-  console.log('request event')
-  res.end('Hello World')
+const server = http.createServer((req,res)=>{
+    console.log('Request event');
+    res.write('Hello World');
+    res.end()
 })
 
-server.listen(5000, () => {
-  console.log('Server listening on port : 5000....')
+server.listen(5000,()=>{
+    console.log('Server Listening on port : 5000....')
 })
